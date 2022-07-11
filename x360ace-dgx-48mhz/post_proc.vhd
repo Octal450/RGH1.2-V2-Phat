b1e70dc2bc9bb4bf5aeb1e39d8242906dcf7cca9
Josh Davidson <joshdavidson2000@outlook.com>
2022-07-11 15:12:04 -0400

    Add stby_clk version

--- x360ace-dgx-48mhz/post_proc.vhd
+++ x360ace-dgx-48mhz/post_proc.vhd
@@ -1,4 +1,4 @@
--- RGH1.2 Code for X360ACE/DGX, Falcon/Jasper Version
+-- RGH1.2 Code for X360ACE/DGX, Falcon/Jasper 48 MHz Version
 -- 15432, GliGli, Octal450
 
 library IEEE;
@@ -21,8 +21,8 @@ end post_proc;
 architecture arch of post_proc is
 
 constant R_LEN : integer := 2;
-constant R_STA : integer := 364387; -- 377 - 397 works for me on various chips and boards, crystal varies slightly per chip, adjust value as needed
-constant T_BUF : integer := 52084;
+constant R_STA : integer := 349839; -- 18 - 21 best
+constant T_BUF : integer := 50000;
 constant T_END : integer := R_STA + R_LEN + T_BUF;
 
 constant post_rgh : integer := 24;
